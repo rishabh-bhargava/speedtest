e8d470e69fcb1661d9d40b34ee9bde81e7c68e1f
rishabh-bhargava <rishabh.bhargava93@gmail.com>
2021-03-27 21:28:16 -0700

    Error handling the situations when Internet is unavailable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from loguru import logger
 import speedtest
+import time
 
 logger.add("speedtest_results.log", format="{message}", rotation="1 hour")
 
@@ -8,17 +10,22 @@ servers = []
 threads = 1
 
 while True:
-    s = speedtest.Speedtest()
-    s.get_servers(servers)
-    s.get_best_server()
-    s.download(threads=threads)
-    s.upload(threads=threads)
-    s.results.share()
+    try:
+        s = speedtest.Speedtest()
+        s.get_servers(servers)
+        s.get_best_server()
+        s.download(threads=threads)
+        s.upload(threads=threads)
+        s.results.share()
 
-    results_dict = s.results.dict()
-    logger.info(
-        '{}|{}|{}',
-        results_dict['timestamp'],
-        results_dict['download'],
-        results_dict['upload']
-    )
+        results_dict = s.results.dict()
+        logger.info(
+            '{}|{}|{}',
+            results_dict['timestamp'],
+            results_dict['download'],
+            results_dict['upload']
+        )
+    except (speedtest.ShareResultsConnectFailure, speedtest.ConfigRetrievalError):
+        timestamp_now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        logger.info('{}|{}|{}', timestamp_now, 0, 0)
+        time.sleep(5)
